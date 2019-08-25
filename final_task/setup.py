cdb39d44efdad0d9d36e4ff3a2fdd5921612d5d9
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2019-08-25 16:25:24 +0300

    Add distribution

---
+++ final_task/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycalc",
+    version="0.0.1",
+    author="Elizabeth Lemeshko",
+    author_email="lzbth.lemeshko@gmail.com",
+    description="Pure-python command-line calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/widbnudb/PythonHomework",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
