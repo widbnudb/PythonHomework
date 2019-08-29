6a3f0ea7f7f0ced368632c36169502ba2d2f93b8
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2019-08-29 16:48:35 +0300

    For checking import in tests

--- final_task/pycalc/test.py
+++ final_task/pycalc/test.py
@@ -1,24 +1,23 @@
 import unittest
-target = __import__("pycalc.pycalc")
-calc = target.functions_evaluation
+from pycalc import pycalc
 
 
 class TestWrongInput(unittest.TestCase):
     def test_comparison(self):
-        self.assertTrue(calc('8*3==6*4'))
-        self.assertTrue(target.functions_evaluation('4/3+1!=4/3+2'))
-        self.assertTrue(target.functions_evaluation('6*(1+3)<7*(1+3)'))
-        self.assertTrue(target.functions_evaluation('-4/-1>4/-1'))
-        self.assertTrue(target.functions_evaluation('7^3<=7^3'))
-        self.assertTrue(target.functions_evaluation('7^2<=7^3'))
-        self.assertTrue(target.functions_evaluation('7%2>=10%3'))
-        self.assertTrue(target.functions_evaluation('11*8-5>=11*(8-5)'))
-        self.assertFalse(target.functions_evaluation('8*3!=6*4'))
-        self.assertFalse(target.functions_evaluation('4/3+1==4/3+2'))
-        self.assertFalse(target.functions_evaluation('6*(1+3)>7*(1+3)'))
-        self.assertFalse(target.functions_evaluation('-4/-1<4/-1'))
-        self.assertFalse(target.functions_evaluation('7^2>=7^3'))
-        self.assertFalse(target.functions_evaluation('11*8-5<=11*(8-5)'))
+        # self.assertTrue(calc('8*3==6*4'))
+        self.assertTrue(pycalc.functions_evaluation('4/3+1!=4/3+2'))
+        # self.assertTrue(target.functions_evaluation('6*(1+3)<7*(1+3)'))
+        # self.assertTrue(target.functions_evaluation('-4/-1>4/-1'))
+        # self.assertTrue(target.functions_evaluation('7^3<=7^3'))
+        # self.assertTrue(target.functions_evaluation('7^2<=7^3'))
+        # self.assertTrue(target.functions_evaluation('7%2>=10%3'))
+        # self.assertTrue(target.functions_evaluation('11*8-5>=11*(8-5)'))
+        # self.assertFalse(target.functions_evaluation('8*3!=6*4'))
+        # self.assertFalse(target.functions_evaluation('4/3+1==4/3+2'))
+        # self.assertFalse(target.functions_evaluation('6*(1+3)>7*(1+3)'))
+        # self.assertFalse(target.functions_evaluation('-4/-1<4/-1'))
+        # self.assertFalse(target.functions_evaluation('7^2>=7^3'))
+        # self.assertFalse(target.functions_evaluation('11*8-5<=11*(8-5)'))
 
 
 if __name__ == '__main__':
